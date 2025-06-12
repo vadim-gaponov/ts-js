b363fbf90f43e485c7aaf1e7184656a3d5ce616b
Vadim Gaponov <vadim.gaponov@russianschool.com>
2025-06-12 10:41:11 -0400

    ...update (c) notes

--- commons/utils.js
+++ commons/utils.js
@@ -11,7 +11,7 @@
 /*!
 MIT License
 
-Copyright (c) 2025 🔨🤖🔧
+Copyright (c) 2025 Sol=
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
