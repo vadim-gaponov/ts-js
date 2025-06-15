68ef990759fbb439109419b18e9a80bf5dc3db52
Vadim Gaponov <vadim.gaponov@russianschool.com>
2025-06-14 22:43:18 -0400

    ...fix marklet

--- obsidian/ObsidianHook.js
+++ obsidian/ObsidianHook.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Obsidian Hook v7.0.1
+// @name         Obsidian Hook v7.0.2
 // @namespace    http://tampermonkey.net/
-// @version      7.0.1
+// @version      7.0.2
 // @description  adds handler for bookmarklet/
 // @source       https://raw.githubusercontent.com/vadim-gaponov/ts-js/main/obsidian/ObsidianHook.js
 // @downloadURL  https://raw.githubusercontent.com/vadim-gaponov/ts-js/main/obsidian/ObsidianHook.js
@@ -26,7 +26,7 @@
 })();
 
 // This is the bookmarlet:
-// javascript:if%28typeof%20document.obsidianHook%21==%27function%27%29%7Balert%28%22ObsidianHook%20is%20not%20active%21%20%28see%20Obsidian%20Hook%20script%29%22%29%3B%7Delse%7Btry%7Balert%28%22ObsidianHook%21%21%21%22%29%3Bdocument.obsidianHook%28%29%3B%7Dcatch%28e%29%7Bif%28e.message&&e.stack%29%7Balert%28%22ERROR%3A%20%27ObsidianHook%27%20failed%21%20%22+e.message%29%3Bconsole.log%28e.stack%29%3B%20%7Delse%7Bconsole.error%28e%29%3B%7D%20%7D%7D
+// javascript:if%28typeof%20document.obsidianHook%21==%27function%27%29%7Balert%28%22ObsidianHook%20is%20not%20active%21%20%28see%20Obsidian%20Hook%20script%29%22%29%3B%7Delse%7Btry%7Bdocument.obsidianHook%28%29%3B%7Dcatch%28e%29%7Bif%28e.message&&e.stack%29%7Balert%28%22ERROR%3A%20%27ObsidianHook%27%20failed%21%20%22+e.message%29%3Bconsole.log%28e.stack%29%3B%20%7Delse%7Bconsole.error%28e%29%3B%7D%20%7D%7D
 //
 
 // <EOF>
